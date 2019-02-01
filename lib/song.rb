8c27306ef4752f2308f09a1252ee1d497adde91f
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-01 04:06:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,8 @@ class Song
   def self.new_by_filename(file_name)
   instance = file_name.split(" - ")
   newsong = self.new(instance[1])
+  newsong.artist_name = instance[0]
+  newsong.save
   binding.pry
   end
   
