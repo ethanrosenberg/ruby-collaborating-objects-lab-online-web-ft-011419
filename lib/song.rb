4127c2923143fbe3856908cab142d7fd72968e01
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-01 04:17:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,13 @@ class Song
   newsong = self.new(instance[1])
   newsong.artist = instance[0]
   return newsong
+  
+  
+  end
+  
+  def name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
   
 end
